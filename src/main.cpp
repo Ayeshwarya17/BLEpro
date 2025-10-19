8c118a8e74166c55a3c200e9221601b199ce53ee
Ayeshwarya <saishwarya968@gmail.com>
2025-10-19 23:56:13 +0530

    added main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,18 @@
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include "BLELogger.h"
 
-#define SERVICE_UUID        "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-
-// Logging control
-bool enableSerialLog = true;
-bool enableBLELog = true;
-int logCount = 0;
-const int maxLogs = 20;
-
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-    if (enableSerialLog) {
-      Serial.println("BLE device connected");
-    }
-  }
-
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-    if (enableSerialLog) {
-      Serial.println("BLE device disconnected");
-    }
-    // Reset log count if you want to allow reconnect and resend
-    logCount = 0;
-  }
-};
+BLELogger logger;
 
 void setup() {
   Serial.begin(115200);
-  if (enableSerialLog) {
-    Serial.println("Starting BLE Logger...");
-  }
-
-  BLEDevice::init("ESP32 Logger");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );
-
-  pCharacteristic->addDescriptor(new BLE2902());
-  pService->start();
-
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->start();
-
-  if (enableSerialLog) {
-    Serial.println("BLE service started. Waiting for connection...");
-  }
+  logger.begin();
 }
 
 void loop() {
-  if (deviceConnected && enableBLELog && logCount < maxLogs) {
-    String log = "Log #" + String(logCount + 1) + ": millis = " + String(millis());
-
-    if (enableSerialLog) {
-      Serial.println(log);
-    }
-
-    pCharacteristic->setValue(log.c_str());
-    pCharacteristic->notify();
-
-    logCount++;
+  if (logger.isConnected()) {
+    String log = "Log #" + String(millis() / 1000) + ": millis = " + String(millis());
+    Serial.println(log);
+    logger.sendLog(log);
     delay(2000);
   }
 }
